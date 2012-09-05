a6c117ab1f29885a080a28389684ddc9c898bf21
Andreas Seltenreich <andreas+git@gate450.dyndns.org>
2012-09-05 14:25:54 +0200

    adopt state of the child vm on local return of catch

--- types.h
+++ types.h
@@ -21,6 +21,8 @@ struct word {
 
 struct vmstate {
   cell *dp;
+  cell *rp;
+  cell *sp;
   struct word *dictionary;
   char *errstr;
   char base;
@@ -28,9 +30,6 @@ struct vmstate {
   int raw : 1;
   int quiet : 1;
   int errno : 14;
-  cell *return_stack;
-  cell *param_stack;
-  cell *dictionary_stack;
 };
 
 #define IS_WORD(c) (c > ' ')
