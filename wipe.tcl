14072c946580105a74a07d9d67985c5433aca7d3
Andreas Seltenreich <andreas+git@gate450.dyndns.org>
2013-09-06 22:10:23 +0200

    wipe.tcl: Get rid of delays.

--- wipe.tcl
+++ wipe.tcl
@@ -2,31 +2,25 @@
 set tty [lindex $argv 0]
 spawn -noecho -open [set port [open $tty "r+"]]
 
-remove_nulls 0
 set timeout 1
 
-fconfigure stdout -translation binary
-
-set timeout 0
 expect -re $
-set timeout 1
 
-send -raw x
-expect f/t/H/?>
-send -raw f
-expect P/H/?>
-send -raw d
+send -raw X
+expect f/t/H/?>$
+send -raw F
+expect P/H/?>$
+send -raw D
 expect "Delete Firmware #"
 send -raw 0
 expect "are you sure?"
 send -raw y
 expect P/H/?>
-send -raw d
+send -raw D
 expect "Delete Firmware #"
 send -raw 1
 expect "are you sure?"
 send -raw y
 expect P/H/?>
-send -raw x
+send -raw X
 expect f/t/H/?>
-
