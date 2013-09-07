0f5c81b6aadc5c42a791252d54713a5d5aa3e1c7
Andreas Seltenreich <andreas+git@gate450.dyndns.org>
2013-09-07 03:24:42 +0200

    Add lancom.h: vendor specific magic values.

---
+++ lancom.h
@@ -0,0 +1,10 @@
+#ifndef LANCOM_H
+#define LANCOM_H
+
+#define LED34 ((volatile unsigned char *)0xb8000014)
+#define LED18 ((volatile unsigned char *)0xb8000010)
+#define CPLD8 ((volatile unsigned char *)0xb8000008)
+#define CPLD4 ((volatile unsigned char *)0xb8000004)
+#define CPLD0 ((volatile unsigned char *)0xb8000000)
+
+#endif
