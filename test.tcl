e792c4610c0bb0d6c6b5146ea62422dd3b268f1e
Andreas Seltenreich <andreas+git@gate450.dyndns.org>
2013-09-07 17:46:55 +0200

    Test new MARKER word.

--- test.tcl
+++ test.tcl
@@ -195,6 +195,16 @@ test { -1  2  2  within . } {0 $}
 test { -1  2 -1  within . } {0 $}
 test {  0 -1  1  within . } {1 $}
 
+
+test { marker oblivious  : oblivion 666 . ; oblivion } {666 $}
+send { oblivious oblivion
+}
+
+expect {
+    timeout { error }
+    -re abort:.*$
+}
+
 send "forget testsuite-marker bye\n"
 
 interact
