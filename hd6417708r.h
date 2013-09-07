919c53aa44eaebd49e66df5454600a0c20523cab
Andreas Seltenreich <andreas+git@gate450.dyndns.org>
2013-09-07 03:09:33 +0200

    Update hd6417708r_h

--- hd6417708r.h
+++ hd6417708r.h
@@ -1,16 +1,20 @@
 #ifndef HD6417708R_H
 #define HD6417708R_H
 
-#define SCTDR ((volatile unsigned char *)0xfffffe86)
-#define SCRDR ((volatile unsigned char *)0xfffffe8a)
-
-#define SCSSR ((volatile unsigned char *)0xfffffe88)
+#define SCSMR ((volatile unsigned char *)0xFFFFFE80)   //  Serial mode register
+#define SCBRR ((volatile unsigned char *)0xFFFFFE82)   //  Bit rate register
+#define SCSCR ((volatile unsigned char *)0xFFFFFE84)   //  Serial control register
+#define SCTDR ((volatile unsigned char *)0xFFFFFE86)   //  Transmit data register
+#define SCSSR ((volatile unsigned char *)0xFFFFFE88)   //  Serial status register
+#define SCRDR ((volatile unsigned char *)0xFFFFFE8A)   //  Receive data register
+#define SCSPTR ((volatile unsigned char *)0xFFFFFF7C)   //  Serial port register
 
 #define TDRE (1<<7)
 #define RDRF (1<<6)
+#define TEND (1<<2)
+
 
 #define PCTR ((volatile unsigned short *)0xffffff76)
 #define PDTR ((volatile unsigned char *)0xffffff78)
-#define SCSPTR ((volatile unsigned char *)0xffffff7C)
 
 #endif
