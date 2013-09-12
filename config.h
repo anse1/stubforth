cfede86ef88dcf1554d6fa55b8085428a040b2c8
Andreas Seltenreich <andreas+git@gate450.dyndns.org>
2013-09-12 23:15:52 +0200

    Fix last merge.

--- config.h
+++ config.h
@@ -5,11 +5,11 @@
 
 #define FORTHNAME "stubforth"
 
-#define PAD_SIZE 1024 /* The size of the PAD (core-ext.m4) alloca'd on
+#define PAD_SIZE 65536 /* The size of the PAD (core-ext.m4) alloca'd on
 		       first use in a VM instance. */
 
-#define RETURN_STACK_SIZE 1024
-#define PARAM_STACK_SIZE 1024
-#define DICTIONARY_SIZE 1024
+#define RETURN_STACK_SIZE 65536
+#define PARAM_STACK_SIZE 65536
+#define DICTIONARY_SIZE 65536
 
 #endif
