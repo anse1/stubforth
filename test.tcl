a1169dcf3312e75b8dda109acd6c07bfc9c2a45f
Andreas Seltenreich <andreas+git@gate450.dyndns.org>
2013-11-23 10:23:05 +0100

    Implement ROLL.

--- test.tcl
+++ test.tcl
@@ -209,6 +209,9 @@ test { 5 7 2constant twocon twocon twocon . . . . } {7 5 7 5 $}
 
 test {  1 1 <> . } {0 $}
 
+test {  1 2 3 4 3 roll . . . . } {1 4 3 2 $}
+test {  1 2 3 4 0 roll . . . . } {4 3 2 1 $}
+
 send "forget testsuite-marker bye\n"
 
 interact
