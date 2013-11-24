6f9253ebf650a7a8f43522ee4f4adc5a85b8982b
Andreas Seltenreich <andreas+git@gate450.dyndns.org>
2013-11-24 13:32:04 +0100

    config.h: shrink return/parameter stack sizes.

--- config.h
+++ config.h
@@ -5,11 +5,11 @@
 
 #define FORTHNAME "stubforth"
 
-#define PAD_SIZE 1024 /* The size of the PAD (core-ext.m4) alloca'd on
+#define PAD_SIZE 256 /* The size of the PAD (core-ext.m4) alloca'd on
 		       first use in a VM instance. */
 
-#define RETURN_STACK_SIZE 1024
-#define PARAM_STACK_SIZE 1024
+#define RETURN_STACK_SIZE 256
+#define PARAM_STACK_SIZE 512
 #define DICTIONARY_SIZE 1024
 
 #endif
