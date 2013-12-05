b2f8bb8cb11ff297b538c8d160d3d2dc06bf82fa
Andreas Seltenreich <andreas+git@gate450.dyndns.org>
2013-12-05 10:26:37 +0100

    gsend.tcl: Set calibration values, increase timeout.

--- gsend.tcl
+++ gsend.tcl
@@ -6,13 +6,38 @@ set fh [open $gcodefile]
 set data [read $fh]
 set lines [split $data \n]
 
-set timeout 30
-spawn -open [set port [open $tty "r+"]]
+set timeout 5
+spawn -noecho -open [set port [open $tty "r+"]]
 
-send "\nhi\n"
+log_user 0
+
+send "\nhi\nhi\n"
 expect \
     timeout { error timeout } \
-    -re {stubforth .*}
+    -re {stubforth}
+
+send {
+    decimal
+    11 xy-max ! \ xy maximum speed (100us/step)
+    10 g-speed ! \ user speed
+    22 xy-jerk !
+    40 z-jerk ! \ z jerk speed (100us/step)
+    2048    164000  xcal 2!
+    2048    164000  ycal 2!
+    200      1250  zcal 2!  \ M8 threads
+    \ 4096  44521  ecal 2!
+    \ 14336   160000 ecal 2!
+    \ 4096 43400  ecal 2!
+    \ 4096 32000  ecal 2!
+    2000 22000  ecal 2! \ measured to free air @ 195°C
+}
+
+send "hi\n"
+expect \
+    timeout { error timeout } \
+    -re {stubforth}
+
+set timeout 300
 
 send "ginterp\n"
 
