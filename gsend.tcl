fc63fd204ca56b203aca46bce9dc56c440985617
Andreas Seltenreich <andreas+git@gate450.dyndns.org>
2013-12-12 17:50:58 +0100

    reprap: Calibrate for new tapped bolt.

--- gsend.tcl
+++ gsend.tcl
@@ -20,8 +20,9 @@ send {
     decimal
     11 xy-max ! \ xy maximum speed (100us/step)
     10 g-speed ! \ user speed
-    22 xy-jerk !
+    40 xy-jerk !
     40 z-jerk ! \ z jerk speed (100us/step)
+    60 xy-accel !
     2048    164000  xcal 2!
     2048    164000  ycal 2!
     200      1250  zcal 2!  \ M8 threads
@@ -29,7 +30,12 @@ send {
     \ 14336   160000 ecal 2!
     \ 4096 43400  ecal 2!
     \ 4096 32000  ecal 2!
-    2000 22000  ecal 2! \ measured to free air @ 195°C
+    \ 2000 22000  ecal 2! \ measured to free air @ 195°C
+    \ 2040 22000  ecal 2! \ +2%
+    \  2080 22000  ecal 2! \ +4%
+    \ 2100 22000  ecal 2! \ +5%
+    8192 68000 ecal 2! \ measured to free air @ 180°C, 0.5mm-Nozzle
+    4000 28000 ecal 2! \ same with tapped bolt
 }
 
 send "hi\n"
